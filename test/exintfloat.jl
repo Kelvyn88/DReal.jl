0a947ad6c0e6a87536bb3d6f3c4d3ac57c84a8de
Soonho Kong <soonhok@cs.cmu.edu>
2016-06-08 08:42:03 -0400

    fix(test/exintfloat.jl): convert a docstring to a comment

--- test/exintfloat.jl
+++ test/exintfloat.jl
@@ -1,18 +1,16 @@
 using DReal
 using Base.Test
 
-"""
-(set-logic QF_NRA)
-(declare-fun x () Int)
-(declare-fun y () Real)
-(assert (<= 10 x))
-(assert (<= x 20))
-(assert (<= 30 y))
-(assert (<= y 66))
-(assert (<= (- (sin x) y) 0.3))
-(check-sat)
-(exit)
-"""
+# (set-logic QF_NRA)
+# (declare-fun x () Int)
+# (declare-fun y () Real)
+# (assert (<= 10 x))
+# (assert (<= x 20))
+# (assert (<= 30 y))
+# (assert (<= y 66))
+# (assert (<= (- (sin x) y) 0.3))
+# (check-sat)
+# (exit)
 
 # x = Var(Int, "x", 10,20)
 # y = Var(Float64, "y", 30.0, 66.)
