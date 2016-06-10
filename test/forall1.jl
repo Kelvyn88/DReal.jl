da9159e8ac8a0a7a386d661a7c6cdb4b2546c45f
Zenna <zennatavares@gmail.com>
2016-06-10 08:49:36 +0100

    forall test

--- test/forall1.jl
+++ test/forall1.jl
@@ -1,7 +1,7 @@
 using DReal
 
-b = ForallVar(global_context(), Float64)
-c = ForallVar(global_context(), Float64)
+b = ForallVar(Float64)
+c = ForallVar(Float64)
 d = Var(Float64,-10.,10.)
 
 f(x,y) = (x + y)*d + 6
